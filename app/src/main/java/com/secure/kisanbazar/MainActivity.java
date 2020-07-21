aed66fc6cb86ea54392e5216de109fa2bba4ea93
SatyamSoni23 <2018kucp1121@iiitkota.ac.in>
2020-07-22 00:21:05 +0530

    login setup

--- app/src/main/java/com/secure/kisanbazar/MainActivity.java
+++ app/src/main/java/com/secure/kisanbazar/MainActivity.java
@@ -2,13 +2,25 @@ package com.secure.kisanbazar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static  int TIME_OUT = 4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final View mainLayout = findViewById(R.id.startScreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, login.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
     }
 }
