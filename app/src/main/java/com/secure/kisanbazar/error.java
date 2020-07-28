e0d33bb495da06f6f451eb41f041c20445f160cc
SatyamSoni23 <2018kucp1121@iiitkota.ac.in>
2020-07-28 22:37:39 +0530

    Add pages

---
+++ app/src/main/java/com/secure/kisanbazar/error.java
@@ -0,0 +1,14 @@
+package com.secure.kisanbazar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class error extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error);
+    }
+}
---
+++ app/src/main/java/com/secure/kisanbazar/registertrader.java
@@ -0,0 +1,14 @@
+package com.secure.kisanbazar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registertrader extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registertrader);
+    }
+}
